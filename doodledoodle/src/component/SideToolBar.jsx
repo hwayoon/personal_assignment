d914731d4c8d4ac17f5f7a99c4fd3328c868c81b
{hwayoon] <{rup_rupes@naver.com}>
2024-04-21 20:26:53 +0900

    리액트 컴포넌트 변경 실패
    나중에 리액트 컴포넌트로 drawing app 만들기를 시도해보기로 하고 우선 js 코드 그대로 사용하기로 한다.

---
+++ doodledoodle/src/component/SideToolBar.jsx
@@ -0,0 +1,3 @@
+export default function SideToolBar() {
+  return <div>SideToolBar</div>;
+}
--- doodledoodle/src/main.jsx
+++ doodledoodle/src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import DrawingBoard from "./component/DrawingBoard.jsx";
-import "./index.css";
+// import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <DrawingBoard />
   </React.StrictMode>
 );
